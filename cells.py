81c02fb304fd72cbf9b47e225f3e8b02ddf7ee64
Wilfred Hughes <me@wilfred.me.uk>
2013-09-01 23:45:39 +0100

    Initial work on displaying expenses in a table

--- cells.py
+++ cells.py
@@ -1,31 +1,33 @@
+import sys
+
 from gi.repository import Gtk
+
+from print_expenses import parse_csv
+
 
 class CellRendererTextWindow(Gtk.Window):
 
-    def __init__(self):
+    def __init__(self, rows):
         Gtk.Window.__init__(self, title="CellRendererText Example")
 
         self.set_default_size(200, 200)
 
-        self.liststore = Gtk.ListStore(str, str)
-        self.liststore.append(["Fedora", "http://fedoraproject.org/"])
-        self.liststore.append(["Slackware", "http://www.slackware.com/"])
-        self.liststore.append(["Sidux", "http://sidux.com/"])
+        self.liststore = Gtk.ListStore(object, str, float)
+        self.liststore.append(["Fedora", "http://fedoraproject.org/", -5.22])
 
         treeview = Gtk.TreeView(model=self.liststore)
 
-        renderer_text = Gtk.CellRendererText()
-        column_text = Gtk.TreeViewColumn("Text", renderer_text, text=0)
-        treeview.append_column(column_text)
+        renderer_date = Gtk.CellRendererText()
+        column_date = Gtk.TreeViewColumn("Date", renderer_date, text=0)
+        treeview.append_column(column_date)
 
-        renderer_editabletext = Gtk.CellRendererText()
-        renderer_editabletext.set_property("editable", True)
+        renderer_desc = Gtk.CellRendererText()
+        column_desc = Gtk.TreeViewColumn("Description", renderer_desc, text=1)
+        treeview.append_column(column_desc)
 
-        column_editabletext = Gtk.TreeViewColumn("Editable Text",
-            renderer_editabletext, text=1)
-        treeview.append_column(column_editabletext)
-
-        renderer_editabletext.connect("edited", self.text_edited)
+        renderer_amount = Gtk.CellRendererText()
+        column_amount = Gtk.TreeViewColumn("Amount", renderer_amount, text=0)
+        treeview.append_column(column_amount)
 
         self.add(treeview)
 
@@ -34,7 +36,15 @@ class CellRendererTextWindow(Gtk.Window):
 
 
 if __name__ == '__main__':
-    win = CellRendererTextWindow()
+    if len(sys.argv) != 2:
+        print "Usage: python print_expenses.py /path/to/csv"
+        sys.exit(1)
+    else:
+        file_name = sys.argv[-1]
+        
+    rows = parse_csv(file_name)
+
+    win = CellRendererTextWindow(rows)
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
     Gtk.main()
